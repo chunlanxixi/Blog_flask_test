41ab44bc026af3e055800abfc5b9cee103cf9bd3
hihicliu <hihicliu@gmail.com>
2021-11-17 23:33:24 -0500

    edit method

--- main.py
+++ main.py
@@ -241,7 +241,7 @@ def add_new_post():
     return render_template("make-post.html", form=form, current_user=current_user)
 
 
-@app.route("/edit-post/<int:post_id>")
+@app.route("/edit-post/<int:post_id>", methods=["POST", "GET"])
 @login_required
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
